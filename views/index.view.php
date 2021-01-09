da19441e0ce1a0c39c5fa4cfeacb0c37fc8af2d7
Daniel <daniflores374@gmail.com>
2021-01-09 16:15:18 -0600

    First commit

---
+++ views/index.view.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<script src="https://kit.fontawesome.com/fdd2e8457c.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/EstilosPaginaInicio.css">
+	
+	<title>ExvotosMexicanos</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<div class="logo izquierda">
+				<a href="#"><img src="img/Em.jpg" alt="" class="imagen_logo"></a>
+				<div class="nombre_header">Láminas y Exvotos Mexicanos</div></a>
+			</div>
+			<div class="derecha">
+				<ul>
+					<li><a href="#"><i class="icono fas fa-home"></i>Inicio</a></li>
+					<li><a href="#"><i class="icono far fa-user"></i>Sobre mi</a></li>
+					<li><a href="#"><i class="icono fas fa-map-marker-alt"></i>Ubicación</a></li>
+					<li><a href="#"><i class="icono fas fa-inbox"></i>Contacto</a></li>
+					<li><a href="#"><i class="icono fas fa-palette"></i>Catálogo</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+
+		<div class="seccion1">
+
+			<div class="hoja1">
+			<div class="intro">
+				<div class="contenido_intro">
+					<h1>Exvotos y Laminas Mexicanos</h1>
+					<p>
+						Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Ipsam optio culpa dolor nihil soluta deserunt, aspernatur nisi, commodi explicabo laborum! Quidem reprehenderit ipsum et fugiat suscipit omnis in dicta optio.
+						Incidunt explicabo atque, iste nostrum. Error laudantium eaque mollitia dolore sit odit obcaecati, eos inventore earum necessitatibus reiciendis quos facilis, impedit tempore possimus explicabo magnam numquam! Earum, eos. Autem, nisi.
+					</p>
+				</div>
+			</div>
+
+			<div class="imagenes">
+				<div class="caja1"></div>
+				<div class="caja2"></div>
+			</div>
+		</div>
+		<a href="#" class="boton1">
+			<div><i class="fas fa-chevron-down"></i></div>
+		</a>
+		</div>
+
+
+		<div class="seccion2">
+				<div class="persona">
+
+					<div class="contenedor_persona">
+						<div class="imagen_persona"></div>
+
+						<div class="sobremi">
+							<h2>Sobre mí</h2>
+							<p>
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe illum consectetur possimus maiores architecto omnis deserunt libero. Quibusdam deserunt quam commodi eos quisquam dicta nemo at sint, sequi illum tenetur.
+								Dolores id, fugit impedit ut rem magnam omnis ipsum facere Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Ex non corporis, iusto dolorum cupiditate expedita.
+							</p>
+
+							<a href="#"><i class="fas fa-chevron-down"></i></a>
+						</div>
+					</div>
+				</div>
+		</div>
+
+
+		<div class="seccion3">
+			<div class="ubicacion">
+				<div class="direccion">
+					<h2>UBICACIÓN</h2>
+					<p>
+						Av. Paseo de la Reforma, Juárez, Cuauhtémoc, 06500 Ciudad de México, CDMX
+					</p>
+				</div>
+
+				<div class="mapa">
+					
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.6654076605945!2d-99.16814652076758!3d19.427020599180462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1609990884167!5m2!1ses-419!2smx" width="750" height="500" frameborder="0" style="border:3px solid ;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+				</div>
+			</div>
+		</div>
+
+
+		<div class="seccion4">
+			<div class="hechos">
+				<div class="box">
+					<div class="contenido_hechos"><i class="far fa-calendar-alt"></i><p>Lorem ipsum, dolor sit amet.</p></div>
+					<div class="contenido_hechos"><i class="fas fa-check"></i><p>Lorem ipsum dolor sit, amet.</p></div>
+					<div class="contenido_hechos"><i class="fas fa-brain"></i><p>Lorem ipsum dolor, sit amet.</p></div>
+					<div class="contenido_hechos"><i class="fab fa-envira"></i><p>Lorem, ipsum dolor sit amet.</p></div>
+				</div>
+
+				<div class="boton_section4">
+					<div class="boton_explorar">
+						<a href="#">Explorar</a>
+					</div>
+				</div>
+
+
+			</div>
+
+			<div class="contacto">
+				<div class="texto_contacto">
+					<h2>Contacto</h2>
+				</div>
+
+				<div class="redes_sociales">
+
+					<div class="contenedor_contacto">
+						<div class="logo_red_face">
+							<a href="https://www.facebook.com/exvotosylaminasMexicanas"><i class="fab fa-facebook-square"></i></a>
+						</div>
+
+						<a href="https://www.facebook.com/exvotosylaminasMexicanas"><p> Láminas y Éxvotos Mexicanos</p></a>
+					</div>
+					<div class="contenedor_contacto">
+						<div class="logo_red_whats">
+							<a href=""><i class="fab fa-whatsapp"></i></a>
+						</div>
+
+						<a href="#"><p>55 7349 5550</p></a>
+					</div>
+					<div class="contenedor_contacto">
+						<div class="logo_red_insta">
+							<a href="https://www.instagram.com/exvotos_mexicanos/"><i class="fab fa-instagram"></i></a>
+						</div>
+
+						<a href="https://www.instagram.com/exvotos_mexicanos/"><p>@exvotos_mexicanos</p></a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+		<footer>
+		
+			<div class="texto_footer">
+				<h2>
+					Laminas y Exvotos <br>
+					Mexicanos
+				</h2>
+			</div>
+			
+
+			<div class="footer_2">
+				<a href="https://www.facebook.com/exvotosylaminasMexicanas">
+					<div class="face">
+						<i class="fab fa-facebook-f"></i>
+					</div>
+				</a>
+
+				<a href="https://www.instagram.com/exvotos_mexicanos/">
+					<div class="insta">
+						<i class="fab fa-instagram"></i>
+					</div>
+				</a>
+			</div>
+
+
+			<div class="pie_pagina">
+				<p>
+					Copyright © 2021 Daniel Flores R. All rights reserved.
+				</p>
+			</div>
+		</footer>
+
+	
+</body>
+</html>
